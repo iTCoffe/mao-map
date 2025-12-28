3ed4bc9e0275327faee713bdc83ef0a814ebfe62
IT咖啡館 <176115520+iTCoffe@users.noreply.github.com>
2025-12-28 11:01:20 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3067,6 +3067,7 @@ const MUSIC_PLAYLIST = [
       //"https://raw.githubusercontent.com/iTCoffe/mao-map/master/data/music/Internationale-cmn_(英特纳雄耐尔).ogg",
       //"https://upload.wikimedia.org/wikipedia/commons/5/5b/Internationale-cmn_(英特纳雄耐尔).ogg",
       "https://iTCoffe.Github.io/mao-map/data/music/Internationale-cmn_(英特纳雄耐尔).ogg",
+      "https://mmap.ifanr.pp.ua/data/music/Internationale-cmn_(英特纳雄耐尔).ogg",
     ],
   },
   {
@@ -3078,6 +3079,7 @@ const MUSIC_PLAYLIST = [
       //"https://raw.githubusercontent.com/iTCoffe/mao-map/master/data/music/东方红_-_The_East_Is_Red_(1950).ogg",
       //"https://upload.wikimedia.org/wikipedia/commons/d/d8/东方红_-_The_East_Is_Red_(1950).ogg",
       "https://iTCoffe.Github.io/mao-map/data/music/东方红-The_East_Is_Red_(1950).ogg",
+      "https://mmap.ifanr.pp.ua/data/music/东方红-The_East_Is_Red_(1950).ogg",
     ],
   },
 ];
